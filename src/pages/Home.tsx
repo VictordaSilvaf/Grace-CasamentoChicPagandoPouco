2a09d92f0f81085ff132d0aa6202bbb4a670866e
Felipe-Alves-Augusto <70119937+Felipe-Alves-Augusto@users.noreply.github.com>
2025-06-01 12:33:48 -0300

    feat new layout plan

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,63 @@
-import Banner from '@/components/HomeComponents/Banner'
-import AchievementsSection from '@/components/HomeComponents/AchievementsSection'
-import HeroSection from '@/components/HomeComponents/HeroSection'
-import Menu from '@/components/HomeComponents/Menu'
-import SlidderSection from '@/components/HomeComponents/SlidderSection'
-import ServicesSection from '@/components/HomeComponents/ServicesSection'
-import DepoinmentsSection from '@/components/HomeComponents/DepoinmentsSection'
-import PartnersSection from '@/components/HomeComponents/PartnersSection'
-import BlogGallery from '@/components/HomeComponents/BlogGallery'
-import CTASection from '@/components/HomeComponents/CTASection'
-import Footer from '@/components/HomeComponents/Footer'
-import TitleSection from '@/components/TitleSection'
-import DescriptionSection from '@/components/DescriptionSection'
-import PartnersLogo from '@/components/HomeComponents/PartnersLogo'
+import Banner from "@/components/HomeComponents/Banner";
+import AchievementsSection from "@/components/HomeComponents/AchievementsSection";
+import HeroSection from "@/components/HomeComponents/HeroSection";
+import Menu from "@/components/HomeComponents/Menu";
+import SlidderSection from "@/components/HomeComponents/SlidderSection";
+import ServicesSection from "@/components/HomeComponents/ServicesSection";
+import DepoinmentsSection from "@/components/HomeComponents/DepoinmentsSection";
+import PartnersSection from "@/components/HomeComponents/PartnersSection";
+import BlogGallery from "@/components/HomeComponents/BlogGallery";
+import CTASection from "@/components/HomeComponents/CTASection";
+import Footer from "@/components/HomeComponents/Footer";
+import TitleSection from "@/components/TitleSection";
+import DescriptionSection from "@/components/DescriptionSection";
+import PartnersLogo from "@/components/HomeComponents/PartnersLogo";
+import PlansSection from "@/components/HomeComponents/PlansSection";
 
 const Home = () => {
-    return (
-        <div className='w-full overflow-x-hidden relative'>
-            <Menu />
+  return (
+    <div className="w-full overflow-x-hidden relative">
+      <Menu />
 
-            <Banner />
+      <Banner />
 
-            <HeroSection />
+      <HeroSection />
 
-            <CTASection />
+      <CTASection />
 
-            {/* Colocar todos os detalhes nessa seção */}
-            <ServicesSection />
+      {/* Colocar todos os detalhes nessa seção */}
+      <ServicesSection />
 
-            {/* + (altura) tamanho, 4 items e deixar igual o site: https://vendatodosantodia.com.br/stories10x/?conversion=ppt-s10x&utm_source=ig-org&utm_medium=bio-ig&utm_campaign=bio-ig&utm_content=s10x-bio */}
-            <DepoinmentsSection />
+      {/* + (altura) tamanho, 4 items e deixar igual o site: https://vendatodosantodia.com.br/stories10x/?conversion=ppt-s10x&utm_source=ig-org&utm_medium=bio-ig&utm_campaign=bio-ig&utm_content=s10x-bio */}
+      <DepoinmentsSection />
 
-            {/* Sobre a grace, texto na laterla e imagem no lado oposto */}
-            <AchievementsSection />
+      {/* Sobre a grace, texto na laterla e imagem no lado oposto */}
+      <AchievementsSection />
 
+      <BlogGallery />
 
-            <BlogGallery />
+      <PartnersLogo />
 
-            <PartnersLogo />
+      {/* Fotos de decoracao */}
+      <SlidderSection />
 
-            {/* Fotos de decoracao */}
-            <SlidderSection />
+      <PlansSection />
 
-            <div className="min-h-[50vh] w-full bg-white">
-                <div className="max-w-7xl mx-auto pb-10">
-                    <TitleSection title="Fale conosco" />
-                    <DescriptionSection>
-                        Fale conosco para tirar suas dúvidas e agendar seu casamento
-                    </DescriptionSection>
-                </div>
-                <PartnersSection />
-            </div>
-
-            {/* <PlansSection /> */}
-
-            {/* <InfoSection /> */}
-
-            <Footer />
+      <div className="min-h-[50vh] w-full bg-white">
+        <div className="max-w-7xl mx-auto pb-10">
+          <TitleSection title="Fale conosco" />
+          <DescriptionSection>
+            Fale conosco para tirar suas dúvidas e agendar seu casamento
+          </DescriptionSection>
         </div>
-    )
-}
+        <PartnersSection />
+      </div>
 
-export default Home
+      {/* <InfoSection /> */}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
